e8d3f69ca42cdbcc47764f86fd3a51274f0dd387
Daniel Pointon <dlp14developing@gmail.com>
2024-10-13 09:53:32 +0000

    Update imgs

--- src/lib/data/mockData.js
+++ src/lib/data/mockData.js
@@ -1,3 +1,5 @@
+let orderNumber = 132;
+
 export const receiptData  = {
     restaurantName: "FISH & CHIPS RESTAURANT",
     address: "1234 Main Street",
@@ -12,14 +14,22 @@ export const receiptData  = {
         { name: "Tartar Sauce", price: 1.99 },
         { name: "Ketchup", price: 0.99 }
     ]
+}
 
+export function generateTableNumber() {
+    return Math.floor(Math.random()*112);
+}
+
+export function getOrderNumber(){
+    orderNumber -= 1;
+    return orderNumber;
 }
 
 export const mockEmployees = [
     {
         favorite: true,
         address: "GBVSEKXHL4JK6A2UUYPC463UQTWXTLLQ4MF246C3Y26GXLTROLSKCTOF",
-        image: "woman1.jpg",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw9JZbsJE4aTF1SIp8Ytk7P7A62HQskbmvgg&s",
         name: "Alice Johnson",
         id: "1"
     },
@@ -27,21 +37,21 @@ export const mockEmployees = [
         favorite: false,
         address: "GAEQOSDIE54ZZMSMVGL5REZ47NBN5GJR46XIWOOA6PXDGRWYZONNOFZB",
         name: "Bob Smith",
-        image: "woman1.jpg",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_YywG_8PeFm6bYNVrfWRS_xBNdNkvHZhM9w&s",
         id: "2"
     },
     {
         favorite: true,
         address: "GD4LI5O4M2C76EDD6GWPPFP2L7AREXGBGQL3KAF2OGTWBD3WYSISM5JD",
         name: "Charlie Brown",
-        image: "woman2.jpg",
+        image: "https://img.freepik.com/premium-photo/young-african-american-waiter-man-hold-tray_521733-6356.jpg?w=740",
         id: "3"
     },
     {
         favourite: false,
         address: "GBAODDLR7OZK75JICEF46UNZ4KS3VF3OBTJNISDEZ73HXKEBL2IJRCD5",
         name: "George Clooney",
-        image: "https://londondrinksguide.com/en/blog/images/w/1270/LondonDrinksGuide-10062022070129000000-633ec399bfc1d.jpg",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSl4E3oJ_zOoBVQZLjry0piHlrc6mKntxxcqw&s",
         id: "4"
     }
 ];
