a7e2c3fdb10aeb2ef1309ac5b6e62667f5211467
Daniel Pointon <dlp14developing@gmail.com>
2024-10-13 11:17:13 +0000

    Add logo

--- src/routes/dashboard/components/SidebarMenu.svelte
+++ src/routes/dashboard/components/SidebarMenu.svelte
@@ -12,12 +12,6 @@
     ]
 </script>
 
-<center>
-    <img src="https://banner2.cleanpng.com/20180410/ggq/avf52f2tf.webp">
-</center>
-
-
-
 <ul class="menu h-full w-80 bg-base-200 p-4 text-base-content">
     {#each dashboardRoutes as route}
         {@const linkClass = $page.route.id === route.route ? 'active': ''}
